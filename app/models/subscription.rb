ea15e1bbd6942d1dd5a4567632b3f2a9a1a62117
= <=>
2022-11-15 17:46:21 +0530

    Constant extraction

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,10 @@
 class Subscription < ApplicationRecord
+  MAXIMUN_USER_COUNT = 4
   has_many :subscribed_users, class_name: 'UserSubscription', dependent: :destroy, counter_cache: :users_count, foreign_key: :subsciption_id, inverse_of: :subscription
   has_many :users, through: :subscribed_users, source: :user
 
   validates :name, :price, :validity, presence: true
-  validates :users_count, numericality: { less_than_or_equal_to: 4 }
+  validates :users_count, numericality: { less_than_or_equal_to: MAXIMUN_USER_COUNT }
   with_options allow_blank: true do
     validates :name, format: { with: Netflix::REGEX_NAME }
     validates :price, :validity, :users_count, numericality: { only_integer: true, greater_than: 0 }
